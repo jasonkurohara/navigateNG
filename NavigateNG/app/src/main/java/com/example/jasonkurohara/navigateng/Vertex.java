4cf22db422598f2839d977d03c9ffa35c91daae1
amuletspade231 <anniemaroon@gmail.com>
2018-08-14 21:17:00 +0000

    Update Vertex.java

--- NavigateNG/app/src/main/java/com/example/jasonkurohara/navigateng/Vertex.java
+++ NavigateNG/app/src/main/java/com/example/jasonkurohara/navigateng/Vertex.java
@@ -10,6 +10,12 @@ public class Vertex extends Graph{
 
     private ArrayList<Vertex> neighbors;
 
+    public Vertex() {
+    	x = y = 0;
+    	name = "";
+    	candidateDistance = 0;
+    }
+    
     public int getX(){
         return x;
     }
@@ -34,23 +40,10 @@ public class Vertex extends Graph{
         y = ycoord;
     }
 
-    public void addNeighbor(Vertex neighbor){
-        neighbors.add(neighbor);
-    }
-
-    public boolean isConnected(  Vertex end ){
-        for( Edge currentEdge: allEdges ){
-            ArrayList<Vertex> connections = new ArrayList<Vertex>();
-            connections = currentEdge.getConnections();
-            if( connections.contains(this) && connections.contains(end) ){
-                return true;
-            }
-        }
-        return false;
-    }
-
     public void addNeighbors(Vertex neighbor){
-        neighbors.add(neighbor);
+        if(neighbor != null) {
+            `neighbors.add(neighbor);
+        }
     }
 
     public void setCandidateDistance(double priority){
