a66f07f690fe4f68e46b7b9d79e384f871765ea6
Jason Kurohara <32893342+jkurohara@users.noreply.github.com>
2018-08-16 23:37:09 -0700

    newest changes puts full path in correct order

---
+++ TesterProgram/src/Edge.java
@@ -0,0 +1,45 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Edge {
+
+    private Vertex start;
+    private Vertex end;
+    private double cost;
+    
+    public Edge() {
+    	start = null;
+    	end = null;
+    	cost = 0;
+    }
+    
+    public Edge(Vertex a, Vertex b, double dist) {
+    	start = a;
+    	end = b;
+    	cost = dist;
+    }
+
+    public void setStart(Vertex a){
+        start = a;
+    }
+
+    public void setEnd(Vertex a){
+        end = a;
+    }
+
+    public void setCost(double weight){
+        cost = weight;
+    }
+
+    public Vertex getStart(){
+        return start;
+    }
+
+    public Vertex getEnd() {
+        return end;
+    }
+
+    public double getCost(){
+        return cost;
+    }
+}
